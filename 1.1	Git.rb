d43bd5357cefcb63faf68b9913c30c9fadac9c35
GiauNV <giaunv@nustechnology.com>
2018-02-06 09:01:58 +0700

    Add git pull description

--- 1.1	Git.rb
+++ 1.1	Git.rb
@@ -1,3 +1,8 @@
 git push origin master #what is origin and can I change it?
 add && commit #compare them
 git commit -a -m "messages" #-a === --all (without add)
+
+git pull = git fetch + git merge
+
+e.g current_branch: abc
+git pull origin abc = git fetch origin + git merge origin/abc
