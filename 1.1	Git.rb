7477fc8641d43e71b9aecf6ddabd17e52716ae69
vothanhbinh111 <binhvt@nustechnology.com>
2018-02-06 11:26:27 +0700

    gitnotes

--- 1.1	Git.rb
+++ 1.1	Git.rb
@@ -6,3 +6,13 @@ git pull = git fetch + git merge(binh + giau changes + changes1)
 
 e.g current_branch: abc
 git pull origin abc = git fetch origin + git merge origin/abc
+
+#merge
+git checkout master
+git merge abc
+
+#rebase
+git checkout abc
+git rebase master
+git checkout master
+git merge abc
