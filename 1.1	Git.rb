77d78895d0372e1e6f0d1f871e57c63cf8214d90
vothanhbinh111 <binhvt@nustechnology.com>
2018-02-06 10:05:12 +0700

    test

--- 1.1	Git.rb
+++ 1.1	Git.rb
@@ -2,7 +2,7 @@ git push origin master #what is origin and can I change it?
 add && commit #compare them
 git commit -a -m "messages" #-a === --all (without add)
 
-git pull = git fetch + git merge
+git pull = git fetch + git merge (binh changes)
 
 e.g current_branch: abc
 git pull origin abc = git fetch origin + git merge origin/abc
