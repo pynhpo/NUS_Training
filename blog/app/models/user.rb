1aed48a55a41ab2a9d0719bb1fdbe4798c828a8c
pynhpo <vothanhbinh0995@gmail.com>
2018-02-27 04:20:17 +0700

    ex_valid

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-	validates :name, :email, :age, presense: true
+	validates :name, :email, :age, presence: true
 	validates :age, numericality: { greater_than: 0 }
-	validates :valid_name
+	validates :email, uniqueness: true
+	validate :valid_name
 
 	def valid_name
 		if name.blank? 
-			errors.add(:name, "can't be blank")
+			errors.add(:name, "can not be blank")
 		end
 	end
 end
